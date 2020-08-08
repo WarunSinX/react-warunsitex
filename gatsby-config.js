27fcf2822ce2612d3855a7e8eadb8e3e30dbbdd6
warunsinx <warunsinx@gmail.com>
2020-08-09 02:52:52 +0700

    Initial cleanup and configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `WarunSiteX`,
+    description: `Warun Singhal is a simple man who love to code.`,
+    author: `warunsinx`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
