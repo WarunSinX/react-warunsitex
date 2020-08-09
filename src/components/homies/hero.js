2a8998f564c9784aa4fc68d10e3affcb5db0a76e
warunsinx <warunsinx@gmail.com>
2020-08-09 20:07:43 +0700

    Add intro text to hero

--- src/components/homies/hero.js
+++ src/components/homies/hero.js
@@ -3,7 +3,11 @@ import React from "react"
 const Hero = () => {
   return (
     <section className="min-h-screen">
-      <p>Hero</p>
+      <div className="pt-40">
+        <p>Hello there, my name is</p>
+        <p>Warun Singhal</p>
+        <p>I build things with Code</p>
+      </div>
     </section>
   )
 }
