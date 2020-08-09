ae55ba90168270d6596978690c089fd9ebd83a71
warunsinx <warunsinx@gmail.com>
2020-08-09 07:45:59 +0700

    Add about section component

---
+++ src/components/homies/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <div className="h-screen">
+      <p>About</p>
+    </div>
+  )
+}
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,15 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/homies/hero"
+import About from "../components/homies/about"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Hero />
+    <div className="container mx-auto">
+      <Hero />
+      <About />
+    </div>
   </Layout>
 )
 
