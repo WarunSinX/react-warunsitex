c0ac21c05a5401628542bc608e6596676d491beb
warunsinx <warunsinx@gmail.com>
2020-08-10 21:11:22 +0700

    Add google font

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,43 @@
 module.exports = {
-    siteMetadata: {
-        title: `WarunSiteX`,
-        description: `Warun Singhal is a simple man who love to code.`,
-        author: `warunsinx`
+  siteMetadata: {
+    title: `WarunSiteX`,
+    description: `Warun Singhal is a simple man who love to code.`,
+    author: `warunsinx`,
+  },
+  plugins: [
+    `gatsby-plugin-smoothscroll`,
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`roboto mono\:400`],
+        display: "swap",
+      },
     },
-    plugins: [
-        `gatsby-plugin-smoothscroll`,
-        `gatsby-plugin-postcss`,
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`
-            }
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-            }
-        }
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
-    ]
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 }
