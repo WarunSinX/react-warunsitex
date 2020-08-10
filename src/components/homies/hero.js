9ceb30e62b1b739722d7a15c84ee9a64ca8a35f0
warunsinx <warunsinx@gmail.com>
2020-08-10 20:07:22 +0700

    hero style modification

--- src/components/homies/hero.js
+++ src/components/homies/hero.js
@@ -2,11 +2,13 @@ import React from "react"
 
 const Hero = () => {
   return (
-    <section className="min-h-screen">
-      <div className="pt-40">
-        <p>Hello there, my name is</p>
-        <p>Warun Singhal</p>
-        <p>I build things with Code</p>
+    <section className="min-h-screen bg-blue">
+      <div className="pt-40 pl-0 sm:pl-32">
+        <p className="text-orange-500 leading-none">Hello there, my name is</p>
+        <p className="text-6xl text-gray-300 py-0 my-0">Warun Singhal</p>
+        <p className="text-6xl text-gray-600 py-0 my-0 leading-none">
+          I build things with Code
+        </p>
       </div>
     </section>
   )
