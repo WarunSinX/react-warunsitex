564e8c3d7f9f5e1ec37de7d50b8494b4a148795e
warunsinx <warunsinx@gmail.com>
2020-08-13 08:39:04 +0700

    Remove secondary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
     extend: {
       colors: {
         primary: "var(--primary)",
-        secondary: "var(--secondary)",
         "main-text": "var(--text-main)",
         "secondary-text": "var(--text-secondary)",
         accent: "var(--accent)",
