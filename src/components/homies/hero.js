6dd4a6f2bd893c410afb76004d0e0e3ba9d48147
warunsinx <warunsinx@gmail.com>
2020-08-23 15:36:12 +0700

    Add animation to hero section

--- src/components/homies/hero.js
+++ src/components/homies/hero.js
@@ -8,23 +8,41 @@ const Hero = () => {
       className="min-h-screen flex items-center justify-center px-6 sm:px-16 lg:px-0 py-24"
     >
       <div>
-        <p className="text-sm sm:text-base text-accent">
+        <p
+          className="text-sm sm:text-base text-accent"
+          data-sal="slide-down"
+          data-sal-delay="1000"
+          data-sal-duration="1500"
+        >
           Hello there, my name is
         </p>
-        <p className="text-4xl md:text-5xl lg:text-6xl text-main-text font-bold leading-tight">
+        <p
+          className="text-4xl md:text-5xl lg:text-6xl text-main-text font-bold leading-tight"
+          data-sal="slide-right"
+          data-sal-delay="1000"
+          data-sal-duration="800"
+        >
           Warun Singhal
         </p>
-        <p className="text-4xl md:text-5xl lg:text-6xl text-secondary-text mod-let-space leading-none">
+        <p
+          className="text-4xl md:text-5xl lg:text-6xl text-secondary-text mod-let-space leading-none"
+          data-sal="slide-left"
+          data-sal-delay="1000"
+          data-sal-duration="800"
+        >
           I convert coffee into code
         </p>
         <div className="text-sm sm:text-base my-16 text-secondary-text">
-          <p>
+          <p data-sal="zoom-in" data-sal-delay="1000" data-sal-duration="900">
             I'm a second year engineering student and also a Full-Stack Dev,{" "}
             <br className="md:block hidden" />
             Mainly use JavaScript and various web technologies シ
           </p>
         </div>
         <button
+          data-sal="slide-up"
+          data-sal-delay="1000"
+          data-sal-duration="2000"
           className="rounded py-3 px-5 border border-accent outline-none"
           onClick={() => scrollTo("#about")}
         >
