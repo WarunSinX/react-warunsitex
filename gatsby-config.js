227fdb34fb0776fe9ea6a6895fec22b056a2c647
warunsinx <warunsinx@gmail.com>
2020-09-04 23:17:51 +0700

    Update manifest info

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,11 +31,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Warun Singhal | WarunSiteX`,
+        short_name: `warunsitex`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#121117`,
+        theme_color: `#fa8231`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
